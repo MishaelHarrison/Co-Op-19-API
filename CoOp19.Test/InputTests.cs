f3a9c57dea079e283b497e34440cbd5120c4f507
MishaelHarrison <mishael.harrison@gmail.com>
2020-04-23 21:50:45 -0500

    added a test

---
+++ CoOp19.Test/InputTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using CoOp19.Dtb;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoOp19.Dtb.Tests
+{
+    public class InputTests
+    {
+        [Fact()]
+        public void AddTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
